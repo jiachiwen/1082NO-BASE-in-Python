7365a566b92981fd525624bc98df830bb6a9f490
jiachiwen <62232277+jiachiwen@users.noreply.github.com>
2020-03-23 15:34:58 +0800

    for迴圈ˋ4
    九九乘法表
    上下左右對齊

--- 0323test4.py
+++ 0323test4.py
@@ -1,3 +1,7 @@
 for m in range(1,10):
     for n in range(1,10):
-        print(m,"*",n,"=",(m*n), end="")
+        if m*n>=10:
+            print(m,"*",n,"=",(m*n), end=" ")
+        else:
+            print(m,"*",n,"=",(m*n), end=" ")
+#我看不懂為什麼最後一行要加 print("")
